ad31acc4633db6a67e0f7697d95a893e5d5b12a2
klack710 <willow710kut@gmail.com>
2018-06-29 15:54:09 +0900

    var_dump追加

--- immutable/main.php
+++ immutable/main.php
@@ -4,5 +4,8 @@ require_once __DIR__.'/ImmClass.php';
 $first = new ImmClass('first', 1);
 $first_2 = $first->setNum(2);
 
+var_dump($first);
+var_dump($first_2);
+
 echo 'first: str->' . $first->getStr() . ', Num->' . $first->getNum() . PHP_EOL;
 echo 'first_2: str->' . $first_2->getStr() . ', Num->' . $first_2->getNum() . PHP_EOL;
