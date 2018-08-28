c0a21f80647c5a7a1b5a5cd9a9a3a2c250ed52ca
klack710 <willow710kut@gmail.com>
2018-08-28 12:57:40 +0900

    facadeパターンの実装

---
+++ facade/app/LegalDepartment.java
@@ -0,0 +1,10 @@
+public class LegalDepartment {
+    public boolean checkLegal(String item_name)
+    {
+        // TODO:法務チェックの具体的な実装
+        if (item_name != "") {
+            return true;
+        }
+        return false;
+    }
+}
